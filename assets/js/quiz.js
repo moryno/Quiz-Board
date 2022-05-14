0f18bc3938a3e61beaa5cf6a3354c1f5c26cc915
Maurice Nganga <61679467+moryno@users.noreply.github.com>
2022-05-14 15:50:10 +0300

    Modify the submit function in js file

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,50 +1,47 @@
 let studentAnswer = [];
 let points = [];
 
+
 // CREATE A FUNCTION TO IDENTIFY THE SELECTED ANSWER AND STORE IT IN A LIST
 let numberOfChoices = document.querySelectorAll(".selected").length;
 for(let i = 0; i < numberOfChoices; i++){
     document.querySelectorAll(".selected")[i].addEventListener("click", function(){
-         let selectedAnswer = this.textContent;
-         
-         if(selectedAnswer === "HTML"){
+         let checked = this.checked;
+         if(checked){
+         let selectedAnswer = this.value;
+         if(selectedAnswer === "html"){
             points.push(10);
-            
         }
         else{
             points.push(23)
         }
+    }
+    else{
+        console.log( "No answer has been selected");
+    }
          
   });
 };
 
 // CREATE A FUNCTION TO COMPUTE AND DISPLAY SCORE WHEN SUBMITTED
-document.querySelector(".submit").addEventListener("click", function(){
+
+
+const onNext = () => {
     let totalPoints = 0;
     for(let i = 0; i < points.length; i++){
         totalPoints += points[i];
-
-        switch (totalPoints) {
-            case "totalPoints >= 80":
-                document.querySelector("h3").textContent = "Exellently passed!!!";
-                document.querySelector("#heading p" ).textContent = "Your score is " + totalPoints;
-                break;
-            case "50 < totalPoints < 80":
-                document.querySelector("h3").textContent = "Fairly passed.";
-                document.querySelector("#heading p" ).textContent = "Your score is " + totalPoints;
-                break;
-        
-            case "totalPoints == 50":
-                document.querySelector("h3").textContent = "Average, you can do better";
-                document.querySelector("#heading p" ).textContent = "Your score is " + totalPoints;
-                break;
-            case "50 < totalPoints":
-                document.querySelector("h3").textContent = "Scored poorly, you need to retake the test";
-                document.querySelector("#heading p" ).textContent = "Your score is " + totalPoints;
-                break;
-        
-            default:
-                break;
+        if(totalPoints >= 80) {
+            document.querySelector("h3").textContent = "Exellently passed!!!";
+            document.querySelector("#heading p" ).textContent = "Your score is " + totalPoints;
         }
+        else if(50 <= totalPoints < 80){
+            document.querySelector("h3").textContent = "Fairly passed.";
+            document.querySelector("#heading p" ).textContent = "Your score is " + totalPoints;
+        }
+        else{
+            document.querySelector("h3").textContent = "Scored poorly, you need to retake the test";
+            document.querySelector("#heading p" ).textContent = "Your score is " + totalPoints;
+        }
+
     }
-})
+}
