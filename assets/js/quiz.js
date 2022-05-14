f94a9ef9881494bf27363b8a2682241b0c9b1be9
Maurice Nganga <61679467+moryno@users.noreply.github.com>
2022-05-14 07:23:40 +0300

    Add a function to find the sum of the total points the student has gotten right

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,11 +1,12 @@
 let studentAnswer = [];
 let points = [];
 
+// CREATE A FUNCTION TO IDENTIFY THE SELECTED ANSWER AND STORE IT IN A LIST
 let numberOfChoices = document.querySelectorAll(".selected").length;
-
 for(let i = 0; i < numberOfChoices; i++){
     document.querySelectorAll(".selected")[i].addEventListener("click", function(){
-         let selectedAnswer = this.innerHTML;
+         let selectedAnswer = this.textContent;
+         
          if(selectedAnswer === "HTML"){
             points.push(10);
             
@@ -17,30 +18,33 @@ for(let i = 0; i < numberOfChoices; i++){
   });
 };
 
+// CREATE A FUNCTION TO COMPUTE AND DISPLAY SCORE WHEN SUBMITTED
 document.querySelector(".submit").addEventListener("click", function(){
     let totalPoints = 0;
     for(let i = 0; i < points.length; i++){
         totalPoints += points[i];
 
         switch (totalPoints) {
-            case ">=80":
+            case "totalPoints >= 80":
                 document.querySelector("h3").textContent = "Exellently passed!!!";
                 document.querySelector("#heading p" ).textContent = "Your score is " + totalPoints;
                 break;
-            case ">50 && <80":
-                document.querySelector("h3").textContent = "Exellently passed!!!";
+            case "50 < totalPoints < 80":
+                document.querySelector("h3").textContent = "Fairly passed.";
                 document.querySelector("#heading p" ).textContent = "Your score is " + totalPoints;
                 break;
         
-            case ">50 && <80":
-                document.querySelector("h3").textContent = "Exellently passed!!!";
+            case "totalPoints == 50":
+                document.querySelector("h3").textContent = "Average, you can do better";
+                document.querySelector("#heading p" ).textContent = "Your score is " + totalPoints;
+                break;
+            case "50 < totalPoints":
+                document.querySelector("h3").textContent = "Scored poorly, you need to retake the test";
                 document.querySelector("#heading p" ).textContent = "Your score is " + totalPoints;
                 break;
         
             default:
                 break;
         }
-        document.querySelector("h3").textContent = "Congratulation!!!";
-        document.querySelector("#heading p" ).textContent = "Your score is " + totalPoints;
     }
 })
