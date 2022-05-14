1be69749737f5ca2b69f12fa0f990ff13cbfe32c
Maurice Nganga <61679467+moryno@users.noreply.github.com>
2022-05-14 22:52:13 +0300

    Add a function to move to the previous quiz when the prevQuiz button is clicked on

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -3,25 +3,25 @@ let points = [];
 
 
 // CREATE A FUNCTION TO IDENTIFY THE SELECTED ANSWER AND STORE IT IN A LIST
-let numberOfChoices = document.querySelectorAll(".selected").length;
-for(let i = 0; i < numberOfChoices; i++){
-    document.querySelectorAll(".selected")[i].addEventListener("click", function(){
-         let checked = this.checked;
-         if(checked){
-         let selectedAnswer = this.value;
-         if(selectedAnswer === "html"){
-            points.push(10);
-        }
-        else{
-            points.push(23)
-        }
-    }
-    else{
-        console.log( "No answer has been selected");
-    }
-         
-  });
-};
+let inputs = document.querySelectorAll(".selected");
+inputs.forEach((input) => { 
+   input.addEventListener("click", function(){
+    let checked = this.checked;
+    if(checked){
+    let selectedAnswer = this.value;
+    if(selectedAnswer === "html"){
+       points.push(10);
+   }
+   else{
+       points.push(23)
+   }
+}
+else{
+   console.log( "No answer has been selected");
+}
+   })
+})
+
 
 // CREATE A FUNCTION TO COMPUTE AND DISPLAY SCORE WHEN SUBMITTED
 
@@ -50,8 +50,8 @@ const onNext = () => {
 
 const track = document.querySelector("#form");
 const slides = Array.from(track.children);
-
-// const nextButton = document.querySelector(".nextQuiz");
+const nextButtons = document.querySelectorAll(".nextQuiz");
+const prevButtons = document.querySelectorAll(".prevQuiz");
 const slideWidth = slides[0].getBoundingClientRect().width;
 
 // 1. Arrange slide next to one another
@@ -60,18 +60,35 @@ const setSlidePosition = (slide, i) => {
 }
 slides.forEach(setSlidePosition);
 
-// 2. Move to the next quiz on the right
-const buttons = document.querySelectorAll(".nextQuiz");
-buttons.forEach((button) => {
+// 3. function to move to both left and right
+const sliderMove = (track, currentSlider, targetSlider) =>{
+    track.style.transform = `translateX(-${targetSlider.style.left})`;
+    currentSlider.classList.remove("currentSlide");
+    targetSlider.classList.add("currentSlide");
+        
+}
+
+// 2. move to the next quiz on the right
+
+nextButtons.forEach((button) => {
     button.addEventListener("click", function(){
         const currentSlider = track.querySelector(".currentSlide");
         const nextSlider = currentSlider.nextElementSibling;
-        const positionChangeBy = nextSlider.style.left;
-        // next slide 
-        track.style.transform = `translateX(-${positionChangeBy})`;
-        currentSlider.classList.remove("currentSlide");
-        nextSlider.classList.add("currentSlide");
+        
+        sliderMove(track, currentSlider, nextSlider)
         
     
     })
-})
+});
+
+// 4. move to the previous quiz on left
+prevButtons.forEach((button) => {
+    button.addEventListener("click", function(){
+        const currentSlider = track.querySelector(".currentSlide");
+        const prevSlider = currentSlider.previousElementSibling;
+        
+        sliderMove(track, currentSlider, prevSlider)
+        
+    
+    })
+});
